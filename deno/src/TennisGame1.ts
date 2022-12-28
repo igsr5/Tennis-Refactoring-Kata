8fcf7ec4ab473621ba8190d5c9731c67f4c8bf35
igsr5 <igsr5.dev@gmail.com>
2022-12-28 16:28:06 +0900

    Add game status flag

--- deno/src/TennisGame1.ts
+++ deno/src/TennisGame1.ts
@@ -1,5 +1,12 @@
 import { TennisGame } from "./TennisGame.ts";
 
+const POINT_NAME: Record<number, string> = {
+  0: "Love",
+  1: "Fifteen",
+  2: "Thirty",
+  3: "Forty",
+};
+
 export class TennisGame1 implements TennisGame {
   private m_score1: number = 0;
   private m_score2: number = 0;
@@ -13,72 +20,41 @@ export class TennisGame1 implements TennisGame {
 
   wonPoint(playerName: string): void {
     if (playerName === "player1") this.m_score1 += 1;
-    // TODO: use `else if` instead of `else` to avoid bugs
     else this.m_score2 += 1;
   }
 
   getScore(): string {
     let score: string = "";
-    let tempScore: number = 0;
 
     const diff = Math.abs(this.m_score1 - this.m_score2);
     const isAdvantage =
       (this.m_score1 >= 4 || this.m_score2 >= 4) && diff === 1;
     const isFinished = (this.m_score1 >= 4 || this.m_score2 >= 4) && diff >= 2;
+    const isContinue = !isAdvantage && !isFinished;
 
-    // NOTE: when both player scores are equal
-    if (this.m_score1 === this.m_score2) {
-      switch (this.m_score1) {
-        case 0:
-          score = "Love-All";
-          break;
-        case 1:
-          score = "Fifteen-All";
-          break;
-        case 2:
-          score = "Thirty-All";
-          break;
-        default:
-          score = "Deuce";
-          break;
+    if (isContinue) {
+      const isDeuce = this.m_score1 >= 3 && this.m_score1 === this.m_score2;
+      if (isDeuce) score = "Deuce";
+      else {
+        if (this.m_score1 === this.m_score2)
+          score = `${POINT_NAME[this.m_score1]}-All`;
+        else
+          score = `${POINT_NAME[this.m_score1]}-${POINT_NAME[this.m_score2]}`;
       }
-      // NOTE: when advantage the game
     } else if (isAdvantage) {
       if (this.m_score1 > this.m_score2) {
         score = "Advantage player1";
       } else {
         score = "Advantage player2";
       }
-      // NOTE: when finish the game
     } else if (isFinished) {
       if (this.m_score1 > this.m_score2) {
         score = "Win for player1";
       } else {
         score = "Win for player2";
       }
-    } else {
-      for (let i = 1; i < 3; i++) {
-        if (i === 1) tempScore = this.m_score1;
-        else {
-          score += "-";
-          tempScore = this.m_score2;
-        }
-        switch (tempScore) {
-          case 0:
-            score += "Love";
-            break;
-          case 1:
-            score += "Fifteen";
-            break;
-          case 2:
-            score += "Thirty";
-            break;
-          case 3:
-            score += "Forty";
-            break;
-        }
-      }
     }
+
     return score;
   }
 }
