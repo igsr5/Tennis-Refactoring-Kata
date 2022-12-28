3a6545fa13ec9f01f9cb6e13cb9f79a64907df4f
igsr5 <igsr5.dev@gmail.com>
2022-12-28 17:40:15 +0900

    Tweak

--- deno/src/TennisGame1.ts
+++ deno/src/TennisGame1.ts
@@ -8,49 +8,59 @@ const POINT_NAME: Record<number, string> = {
 };
 
 export class TennisGame1 implements TennisGame {
-  private m_score1: number = 0;
-  private m_score2: number = 0;
-  private player1Name: string;
-  private player2Name: string;
+  private readonly player1Name: string;
+  private readonly player2Name: string;
+  private readonly score: Map<string, number> = new Map();
 
   constructor(player1Name: string, player2Name: string) {
     this.player1Name = player1Name;
     this.player2Name = player2Name;
+    this.score.set(player1Name, 0);
+    this.score.set(player2Name, 0);
   }
 
   wonPoint(playerName: string): void {
-    if (playerName === "player1") this.m_score1 += 1;
-    else this.m_score2 += 1;
+    this.score.set(playerName, this.score.get(playerName)! + 1);
+  }
+
+  /**
+   * @returns [player1Score, player2Score]
+   */
+  getCurrentScore(): [number, number] {
+    return [
+      this.score.get(this.player1Name)!,
+      this.score.get(this.player2Name)!,
+    ];
   }
 
   getCurrentWinner(): string {
-    if (this.m_score1 > this.m_score2) return this.player1Name;
-    else if (this.m_score1 < this.m_score2) return this.player2Name;
-    else return "";
+    const [p1Score, p2Score] = this.getCurrentScore();
+    if (p1Score > p2Score) return this.player1Name;
+    if (p1Score < p2Score) return this.player2Name;
+    return "";
   }
 
   getScoreDiff(): number {
-    return Math.abs(this.m_score1 - this.m_score2);
+    const [p1Score, p2Score] = this.getCurrentScore();
+    return Math.abs(p1Score - p2Score);
   }
 
   isFinished(): boolean {
-    return (
-      (this.m_score1 >= 4 || this.m_score2 >= 4) && this.getScoreDiff() >= 2
-    );
+    const [p1Score, p2Score] = this.getCurrentScore();
+    return (p1Score >= 4 || p2Score >= 4) && this.getScoreDiff() >= 2;
   }
 
   isAdvantage(): boolean {
-    return (
-      (this.m_score1 >= 4 || this.m_score2 >= 4) && this.getScoreDiff() === 1
-    );
+    const [p1Score, p2Score] = this.getCurrentScore();
+    return (p1Score >= 4 || p2Score >= 4) && this.getScoreDiff() === 1;
   }
 
   isDuece(): boolean {
-    return this.m_score1 >= 3 && this.getScoreDiff() === 0;
+    return this.score.get(this.player1Name)! >= 3 && this.getScoreDiff() === 0;
   }
 
   getScore(): string {
-    const diff = this.getScoreDiff();
+    const [p1Score, p2Score] = this.getCurrentScore();
     switch (true) {
       case this.isAdvantage():
         return `Advantage ${this.getCurrentWinner()}`;
@@ -58,10 +68,10 @@ export class TennisGame1 implements TennisGame {
         return `Win for ${this.getCurrentWinner()}`;
       case this.isDuece():
         return "Deuce";
-      default:
-        return diff === 0
-          ? `${POINT_NAME[this.m_score1]}-All`
-          : `${POINT_NAME[this.m_score1]}-${POINT_NAME[this.m_score2]}`;
+      default: // game is continued
+        return this.getScoreDiff() === 0
+          ? `${POINT_NAME[p1Score]}-All`
+          : `${POINT_NAME[p1Score]}-${POINT_NAME[p2Score]}`;
     }
   }
 }
